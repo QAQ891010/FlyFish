4f14508db27a1bc594704e2062e0d12788184619
Administrator <316280551@qq.com>
2018-12-20 11:58:23 +0800

    2.0version

---
+++ app/src/main/java/com/example/administrator/flyfish/Fragment/Fragment_Home.java
@@ -0,0 +1,20 @@
+package com.example.administrator.flyfish.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrator.flyfish.R;
+
+public class Fragment_Home extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.title,container,false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/administrator/flyfish/MainActivity.java
+++ app/src/main/java/com/example/administrator/flyfish/MainActivity.java
@@ -5,10 +5,10 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.administrator.flyfish.Fragment.Fragment_Friend;
+import com.example.administrator.flyfish.Fragment.Fragment_Home;
 import com.example.administrator.flyfish.Fragment.Fragment_User;
 
 
@@ -16,9 +16,11 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView TV_poi;
     private TextView TV_user;
+    private TextView TV_deal;
 
     private Fragment_Friend fragment_friend;
     private Fragment_User fragment_user;
+    private Fragment_Home fragment_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +36,20 @@ public class MainActivity extends AppCompatActivity {
         //实例化Fragment
         fragment_friend = new Fragment_Friend();
         fragment_user = new Fragment_User();
-        
+        fragment_home = new Fragment_Home();
 
 
-        TV_poi= findViewById(R.id.txt_poi);
+        TV_deal = findViewById(R.id.txt_deal);
+        TV_deal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.Fl_main,fragment_home).commitAllowingStateLoss();
+            }
+        });
+
+
+        TV_poi = findViewById(R.id.txt_poi);
         TV_poi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
