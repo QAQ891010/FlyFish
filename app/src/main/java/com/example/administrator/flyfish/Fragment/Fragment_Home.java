b07eb3f49cb4961ea331f238525be5f1bd9bebc2
Administrator <316280551@qq.com>
2018-12-23 14:33:48 +0800

    4.0

--- app/src/main/java/com/example/administrator/flyfish/Fragment/Fragment_Home.java
+++ app/src/main/java/com/example/administrator/flyfish/Fragment/Fragment_Home.java
@@ -1,17 +1,22 @@
 package com.example.administrator.flyfish.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.administrator.flyfish.R;
 import com.example.administrator.flyfish.School;
+import com.example.administrator.flyfish.SchoolActivity;
 import com.example.administrator.flyfish.SchoolAdapter;
 
 import java.util.ArrayList;
@@ -22,6 +27,8 @@ public class Fragment_Home extends Fragment {
     private List<School> schoolList = new ArrayList<>();
     private RecyclerView recyclerView;
 
+    private ListView listView;
+
     private Button btn_change;
 
     @Nullable
@@ -29,11 +36,6 @@ public class Fragment_Home extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.title,container,false);
 
-
-        initSchool();
-        recyclerView = view.findViewById(R.id.recycler_view);
-        SchoolAdapter adapter = new SchoolAdapter(schoolList);
-        recyclerView.setAdapter(adapter);
         return view;
 
     }
@@ -41,8 +43,35 @@ public class Fragment_Home extends Fragment {
     private void initSchool() {
 
 
-        School nanda = new School("江西师范大学",R.drawable.ncdax);
+        School nanda = new School("南昌大学",R.drawable.ncdax);
         schoolList.add(nanda);
+        School JXSD = new School("江西师范大学",R.drawable.jxsc);
+        schoolList.add(JXSD);
+        School HDJD = new School("华东交通大学",R.drawable.hdjd);
+        schoolList.add(HDJD);
+        School JXCJ = new School("江西财经大学",R.drawable.jxcj);
+        schoolList.add(JXCJ);
+        School NCHK = new School("南昌航空大学",R.drawable.nchk);
+        schoolList.add(NCHK);
+        School JXLG = new School("江西理工大学",R.drawable.jxlg);
+        schoolList.add(JXLG);
+        School DHLG = new School("东华理工大学",R.drawable.dhlg);
+        schoolList.add(DHLG);
+        School JXND = new School("江西农业大学",R.drawable.jxny);
+        schoolList.add(JXND);
+        School ZYYD = new School("江西中医药大学",R.drawable.jxzyy);
+        schoolList.add(ZYYD);
+        School YCXY = new School("宜春学院",R.drawable.ycxy);
+        schoolList.add(YCXY);
+        School NCLG = new School("南昌理工学院",R.drawable.nclg);
+        schoolList.add(NCLG);
+
+
+
+
+
+
+
 
 
 
@@ -64,5 +93,18 @@ public class Fragment_Home extends Fragment {
 //                startActivity(intent);
 //            }
 //        });
+
+
+        initSchool();
+        recyclerView = (RecyclerView) view.findViewById(R.id.list_view_school);
+//        recyclerView.setAdapter(new SchoolAdapter(schoolList));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        SchoolAdapter adapter = new SchoolAdapter(schoolList);
+        recyclerView.setAdapter(adapter);
+
+
     }
+
+
 }
